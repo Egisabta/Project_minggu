ed5d333ff6bf7769afef85abac03f45ed0d4da39
FlutLab.io <>
2022-04-09 17:36:42 +0000

    Autosave

--- lib/draw.dart
+++ lib/draw.dart
@@ -111,6 +111,7 @@ class Portofolio extends StatelessWidget {
             height: 150,
             color: Colors.blue,
           ),
+          MainAxisAlignment.spaceAround,
           Container(
             margin: EdgeInsets.all(10),
             width: 150,
