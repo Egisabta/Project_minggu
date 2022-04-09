97dcac709b620dd3dd183b27cd6bbb3f8b3270c1
FlutLab.io <>
2022-04-09 08:48:46 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ class MyApp extends StatelessWidget {
           drawer: DrawerWidget(),
           body: TabBarView(children: <Widget>[
             Home(),
-            Body(),
           ]),
         ),
       );
