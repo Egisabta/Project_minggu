da9e6cf799c7c76f3e3985597babc1aa8d66108b
FlutLab.io <>
2022-04-10 05:59:42 +0000

    Autosave

--- lib/draw.dart
+++ lib/draw.dart
@@ -84,19 +84,19 @@ class Profil extends StatelessWidget {
           ),
           Text(
             'Nama :Egi Sabta Hiro',
-            style: TextStyle(fontWeight: FontWeight.bold, height: 5, fontSize: 60),
+            style: TextStyle(fontWeight: FontWeight.bold, height: 5, fontSize: 20),
           ),
           Text(
             'Kelas : 2A',
-            style: TextStyle(fontWeight: FontWeight.bold, height: 5, fontSize: 60),
+            style: TextStyle(fontWeight: FontWeight.bold, height: 5, fontSize: 20),
           ),
           Text(
             'NIM  : 362055401002',
-            style: TextStyle(fontWeight: FontWeight.bold, height: 5, fontSize: 60),
+            style: TextStyle(fontWeight: FontWeight.bold, height: 5, fontSize: 20),
           ),
           Text(
             'Prodi : Teknik Informatika',
-            style: TextStyle(fontWeight: FontWeight.bold, height: 5, fontSize: 60),
+            style: TextStyle(fontWeight: FontWeight.bold, height: 5, fontSize: 20),
           ),
         ],
       ),
