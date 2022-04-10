110d3dedf6924f74a3e80c1dcbb15fbe64878f96
FlutLab.io <>
2022-04-10 05:20:00 +0000

    Autosave

--- lib/draw.dart
+++ lib/draw.dart
@@ -145,8 +145,9 @@ class Keterangan extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: <Widget>[
-          Text('Information Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'),
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[ 
+        Text('keterangan'),
         ],
       ),
     );
@@ -157,10 +158,10 @@ class Pengaturan extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Align(
-        child: Text('Ini Adalah Background'),
-        alignment: Alignment.center,
-      ),
+      body: Column(
+        children: <Widget>[ 
+        Text('Information Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'),
+        ],
     );
   }
 }
